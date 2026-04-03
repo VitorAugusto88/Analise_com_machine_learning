6215f50b6f1828aad9dbd336c1094a9559e16e9b
VitorAugusto88 <visilvavidal@gmail.com>
2026-04-03 13:30:17 -0300

    first commit

---
+++ Src/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+from pydantic import BaseModel
+import os
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = joblib.load("modelo.pkl")
+
+
+class CasaSimples(BaseModel):
+    rm: float
+
+
+medias = {
+    "crim": 3.61,
+    "zn": 11.36,
+    "indus": 11.13,
+    "chas": 0,
+    "nox": 0.55,
+    "rm": 6.28,
+    "age": 68.5,
+    "dis": 3.79,
+    "rad": 9.5,
+    "tax": 408,
+    "ptratio": 18.45,
+    "b": 356,
+    "lstat": 12.65
+}
+
+@app.post("/prever")
+def prever(dados: CasaSimples):
+    entrada = medias.copy()
+    entrada["rm"] = dados.rm
+
+    df = pd.DataFrame([entrada])
+    previsao = model.predict(df)
+
+    return {"preco": float(previsao[0])}
+
+# teste
+@app.get("/")
+def home():
+    return {"message": "API funcionando"}
